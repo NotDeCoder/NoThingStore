8a088b373bd13ca694d6ce71ac0287503caf7e1d
NotDeCoder <pavlo.kulakov@nure.ua>
2023-04-02 18:44:47 +0300

    Initial commit

---
+++ NoThingStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoThingStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
